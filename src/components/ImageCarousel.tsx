d7928f4703fb6338114da4b1f12830705cac2030
Roman Gordon <roman.gordon@student.kdg.be>
2024-11-16 22:02:32 +0100

    Game details store page has all the required details, and carousel now pops out images when you press on them

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Box, Paper, Stack} from "@mui/material";
+import {Backdrop, Box, Paper, Stack} from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 
 interface ImageCarouselProps {
@@ -11,15 +11,25 @@ interface ScreenshotProps {
 }
 
 export function ImageCarousel({images}: ImageCarouselProps) {
-    const [shownItem, setShownItem] = useState(0)
+    const [shownItem, setShownItem] = useState(0);
+    const [open, setOpen] = useState(false);
+    const [imageClickedUrl, setImageClickedUrl] = useState('');
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
     return (
         <div>
             <Box sx={{marginLeft: "5%", marginRight: "5%", marginTop: "3%"}}>
-                <Carousel sx={{width: "90%"}}
+                <Carousel sx={{width: "100%"}}
                           next={ (active) => {setShownItem(active ? active : 0)} }
                           prev={ (active) => {setShownItem(active ? active : 0)} }
                           interval={10000}
+                          stopAutoPlayOnHover={false}
                           activeIndicatorIconButtonProps={{
                               style: {
                                   color: 'lightgray'
@@ -40,10 +50,18 @@ export function ImageCarousel({images}: ImageCarouselProps) {
                     }
                 </Carousel>
             </Box>
+
+            <Backdrop
+                sx={(theme) => ({color: '#fff', zIndex: theme.zIndex.drawer + 1})}
+                open={open}
+                onClick={handleClose}
+            >
+                <img src={imageClickedUrl} alt={"Game Screenshot"} style={{height: "100%"}}/>
+            </Backdrop>
         </div>
     )
 
-    function Item({url} : ScreenshotProps)
+    function Item({url}: ScreenshotProps)
     {
         const previousCard = shownItem - 1 < 0 ? images.length - 1 : shownItem - 1
         const nextCard = shownItem + 1 > images.length - 1 ? 0 : shownItem + 1
@@ -60,33 +78,51 @@ export function ImageCarousel({images}: ImageCarouselProps) {
                        },
                    }}>
                 {images.length >= 2 && (
-                    <Paper sx={{
-                        width: { xs: '100%', md: '31%' },
-                        height: '300px',
-                        marginBottom: "1%",
-                        marginLeft: { xs: 0, md: '1%'},
-                        marginRight: { xs: 0, md: '1%'},
-                    }}>
+                    <Paper
+                        sx={{
+                            width: { xs: '100%', md: '31%' },
+                            height: '300px',
+                            marginBottom: "1%",
+                            marginLeft: { xs: 0, md: '1%'},
+                            marginRight: { xs: 0, md: '1%'},
+                        }}
+                        onClick={() => {
+                            setImageClickedUrl(images[previousCard])
+                            handleOpen()
+                        }}
+                    >
                         <img src={images[previousCard]} alt={"Previous Screenshot"}/>
                     </Paper>
                 )}
-                <Paper sx={{
-                    width: { xs: '100%', md: '31%' },
-                    height: '300px',
-                    marginBottom: "1%",
-                    marginLeft: { xs: 0, md: '1%'},
-                    marginRight: { xs: 0, md: '1%'},
-                }}>
-                    <img src={url} alt={"Current screenshot"}/>
-                </Paper>
-                {images.length >= 3 && (
-                    <Paper sx={{
+                <Paper
+                    sx={{
                         width: { xs: '100%', md: '31%' },
                         height: '300px',
                         marginBottom: "1%",
                         marginLeft: { xs: 0, md: '1%'},
                         marginRight: { xs: 0, md: '1%'},
-                    }}>
+                    }}
+                    onClick={() => {
+                        setImageClickedUrl(url)
+                        handleOpen()
+                    }}
+                >
+                    <img src={url} alt={"Current screenshot"}/>
+                </Paper>
+                {images.length >= 3 && (
+                    <Paper
+                        sx={{
+                            width: { xs: '100%', md: '31%' },
+                            height: '300px',
+                            marginBottom: "1%",
+                            marginLeft: { xs: 0, md: '1%'},
+                            marginRight: { xs: 0, md: '1%'},
+                        }}
+                        onClick={() => {
+                            setImageClickedUrl(images[nextCard])
+                            handleOpen()
+                        }}
+                    >
                         <img src={images[nextCard]} alt={"Next Screenshot"}/>
                     </Paper>
                 )}
