7b197c8b8a034e89b45f0c86f205b7c1d7c8a13d
DeanTerneu <dean.terneu@student.kdg.be>
2024-11-19 14:17:06 +0100

    implement review changes

--- src/hooks/gameRegistry/useGameDetails.ts
+++ src/hooks/gameRegistry/useGameDetails.ts
@@ -1,7 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
 import {getGameDetails} from "../../services/gameRegistryService.ts";
-import axios from "axios";
-import {Game} from "../../model/gameRegistry/Game.ts";
 
 export function useGameDetails(gameId: string) {
     const {isLoading, isError, data: game} = useQuery({
@@ -14,20 +12,4 @@ export function useGameDetails(gameId: string) {
         isError,
         game
     }
-}
-
-const GAME_REGISTRY_BASE_URL = import.meta.env.VITE_GAME_REGISTRY_URL;
-
-export async function getGamesOverview() {
-    const url = GAME_REGISTRY_BASE_URL + "/games/overview"
-    const {data: games} = await axios.get<Game[]>(`${url}`)
-
-    return games
-}
-
-export async function getGamesOverviewByTitleLikeAndPriceBelow(title: string, maxPrice: string) {
-    const url = GAME_REGISTRY_BASE_URL + `/games/overview?title=${title}&maxPrice=${maxPrice}`
-    const {data: games} = await axios.get<Game[]>(`${url}`)
-
-    return games
 }
