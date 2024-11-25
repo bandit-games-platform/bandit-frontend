78098e2aaaf12f7b3fe742bd1c639feda97abf1e
DeanTerneu <dean.terneu@student.kdg.be>
2024-11-25 11:43:54 +0100

    removing unused method

--- src/hooks/useGamesOverview.ts
+++ src/hooks/useGamesOverview.ts
@@ -1,20 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
 import {getGamesOverviewByTitleLikeAndPriceBelow} from "../services/gameRegistryService.ts";
-import {getGamesOverview} from "./gameRegistry/useGameDetails.ts";
-
-export function useGamesOverview() {
-    const {isLoading, isError, data: overview} = useQuery({
-        queryKey: ['games'],
-        queryFn: () => getGamesOverview(),
-    })
-
-    return {
-        isLoading,
-        isError,
-        overview
-    }
-}
-
 
 export function useGamesOverviewByTitleLikeAndPriceBelow(title: string, maxPrice: number) {
     const {isLoading, isError, data: overview} = useQuery({
