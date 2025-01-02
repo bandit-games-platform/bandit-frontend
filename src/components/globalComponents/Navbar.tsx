b9184a3d973f74c98539f18f886d39051a1bd276
Ayodeji ALLI-SMITH <ayodeji.allismith@student.kdg.be>
2025-01-02 01:28:17 +0100

    testing webhook and latest3

--- src/components/globalComponents/Navbar.tsx
+++ src/components/globalComponents/Navbar.tsx
@@ -74,7 +74,7 @@ export function Navbar() {
                             textDecoration: 'none',
                         }}
                     >
-                        BanditGamesed2
+                        BanditGamesed3
                     </Typography>
 
                     <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
