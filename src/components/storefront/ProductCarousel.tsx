e82e1e0864b641edfd26823fd17c5cf996f7cac6
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2025-01-03 17:08:25 +0100

    fixed the navigation arrows style

--- src/components/storefront/ProductCarousel.tsx
+++ src/components/storefront/ProductCarousel.tsx
@@ -3,6 +3,7 @@ import Carousel from "react-material-ui-carousel";
 import {ProductCarouselItem} from "./ProductCarouselItem.tsx";
 import {Product} from "../../model/storefront/Product.ts";
 import {Box} from "@mui/material";
+import theme from "../../theme/theme.ts";
 
 interface ProductCarouselProps {
     products: Product[]
@@ -13,8 +14,15 @@ export function ProductCarousel({products}: ProductCarouselProps) {
 
     return (
         <div>
-            <Box sx={{marginLeft: "5%", marginRight: "5%", marginTop: "3%"}}>
-                <Carousel sx={{width: "100%"}}
+            <Box sx={{
+                marginLeft: "5%",
+                marginRight: "5%",
+                marginTop: "3%",
+            }}>
+                <Carousel sx={{
+                    width: "100%",
+                    overflow: 'visible',
+                }}
                           next={(active) => {
                               setShownItem(active ? active : 0)
                           }}
@@ -25,7 +33,7 @@ export function ProductCarousel({products}: ProductCarouselProps) {
                           stopAutoPlayOnHover={false}
                           activeIndicatorIconButtonProps={{
                               style: {
-                                  color: 'lightgray'
+                                  color: theme.palette.secondary.light,
                               }
                           }}
                           indicatorIconButtonProps={{
@@ -37,8 +45,29 @@ export function ProductCarousel({products}: ProductCarouselProps) {
                           autoPlay={products.length > 3}
                           indicators={products.length > 3}
                           navButtonsAlwaysInvisible={products.length <= 3}
+                          navButtonsAlwaysVisible={true}
                           onChange={(index) => {
                               setShownItem(index ?? 0); // fallback value of 0 if index is undefined
+                          }}
+
+                          navButtonsWrapperProps={{
+                              style: {
+                                  position: 'absolute',
+                                  top: '50%',
+                                  left: '-50px',
+                                  transform: 'translateY(-50%)',
+                                  zIndex: -0,
+                              }
+                          }}
+                          navButtonsProps={{
+                              style: {
+                                  backgroundColor: "rgb(0, 0, 0, 0.85)",
+                                  position: 'absolute',
+                                  top: '50%',
+                                  right: '-50px',
+                                  transform: 'translateY(-50%)',
+                                  color: theme.palette.secondary.light,
+                              },
                           }}
                 >
                     {
